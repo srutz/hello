8cf77d56c482ed01fe69fb15db37c308b502f6d5
stepan.rutz <stepan.rutz@gmx.de>
2026-02-24 15:17:32 +0100

    useQuotes with tanstack-query

---
+++ src/hooks/useQuote.ts
@@ -0,0 +1,39 @@
+
+import { useState, useEffect } from "react"
+import type { Quote } from "../App"
+import { useQuery } from "@tanstack/react-query"
+
+/* custom hook lädt eine Quote */
+export function useQuoteSchlecht(qid: number) {
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState<Error|null>(null)
+  const [ quote, setQuote ] = useState<Quote|null>(null)
+  useEffect(() => {
+    (async () => {
+      setIsLoading(true)
+      try {
+        const response = await fetch("https://dummyjson.com/quotes/" 
+            + encodeURIComponent(qid))
+        const data = await response.json()
+        setQuote(data as Quote)
+      } catch (e) {
+        setError(e as Error)
+      } finally {
+        setIsLoading(false)
+      }
+    })()
+  }, [qid])
+  return { quote, isLoading, error };
+}
+
+export function useQuote(qid: number) {
+  return useQuery({
+    queryKey: [ "quote", qid],
+    queryFn: async () => {
+      const response = await fetch("https://dummyjson.com/quotes/" 
+            + encodeURIComponent(qid))
+        return await response.json() as Quote
+    },
+    staleTime: 60_000,  
+  })
+}
