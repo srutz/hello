dc1d88a80818fd6ee37a80abe9d2aa790c57d97a
stepan.rutz <stepan.rutz@gmx.de>
2026-02-24 11:42:49 +0100

    Box component, header component und tailwind merge

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,47 @@
-import { Counter } from "./Counter"
+import { useState, type ComponentProps, type ComponentPropsWithoutRef, type MouseEvent, type ReactNode } from "react"
+import { cn } from "./util"
+
+export default App
+
+
 
 export function App() {
+  const [cities, setCities] = useState<string[]>([])
+  const handleClick = () => {
+    setCities([ "london", "paris", "amsterdam" ])
+  }
+
   return (
-    <div className="w-screen h-screen bg-white">
-      <Counter></Counter>
-      <Counter></Counter>
-      <Counter></Counter>
-      <Counter></Counter>
+    <div className="w-screen h-screen bg-white flex 
+        flex-col items-center" onClick={handleClick}>
+      <Box className="text-green-600 shadow-sm font-bold" title="hello"
+        >
+          <H1 className="text-lg">Städte</H1>
+          {cities.map((city) => (<div>{city}</div>))}
+      </Box>
     </div>
   )
 }
 
-export default App
+/* Komponente die von außen ansteuerbar ist */
+function Box({ children, className, ...props }
+  : 
+  { children?: ReactNode} & ComponentProps<"input">) 
+{
+  return (
+    <div className={cn(
+        "bg-zinc-200 rounded-xl p-2 m-2 border border-zinc-400 shadow-xl",
+        className)}
+      {...props}
+      >
+        {children}
+    </div>
+  )
+}
+
+function H1({children, className, ...rest} 
+  : { children: ReactNode} & ComponentProps<"h1">) {
+  return (
+    <h1 className={cn("text-4xl", className)} {...rest}>{children}</h1>
+  )
+}
