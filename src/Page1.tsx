a6bba870f55e3c9c044648ee51ab6c81b833900d
stepan.rutz <stepan.rutz@gmx.de>
2026-02-25 14:17:43 +0100

    form pages

--- src/Page1.tsx
+++ src/Page1.tsx
@@ -1,8 +1,40 @@
+import { useEffect, useState } from "react"
+
 export function Page1() {
+    const [ emailChanged, setEmailChanged] = useState(false)
+
+    const [ email, setEmail] = useState("");
+    const [ phone, setPhone ] = useState("")
+    
+    useEffect(() => {
+        console.log("page1 mounted")
+        return () => {
+            console.log("page1 unmounted")
+        }
+    }, [])
     return (
-        <div className="p-8">
-            <h1 className="text-2xl font-bold mb-4">Page 1</h1>
-            <p>This is the content of Page 1.</p>
+        <div className="flex flex-col gap-1 p-2">
+            <div className="flex gap-1 items-baseline">
+                <label className="w-48" htmlFor="phone">E-Mail</label>
+                <input placeholder = "Your mail" 
+                    onChange={(e) => {
+                        setEmail(e.target.value)
+                        setEmailChanged(true)
+                    }}
+                    type="email"
+                    id="email" value={email}></input>
+            </div>
+            {emailChanged && !email.includes("@") && (
+                <div className="text-red-400">
+                    Invalid E-Mail
+                </div>)}
+            <div className="flex gap-1 items-baseline">
+                <label className="w-48" htmlFor="phone">Phone</label>
+                <input placeholder = "Your number" 
+                    onChange={(e) => setPhone(e.target.value)}
+                    type="tel"
+                    id="phone" value={phone}></input>
+            </div>
         </div>
     )
 }
